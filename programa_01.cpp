bec33c619581566c33a88059ac246fd46d278c26
José Henrique <hf230992@gmail.com>
2021-11-27 14:22:03 -0300

    funçoes_c++
    up

---
+++ programa_01.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include <vector>
+    using std::cout;
+    using std::endl;
+
+//prototipo 
+int soma(int, int ); 
+int areaQuadrado(int, int);
+   
+int main(){
+    cout << "Soma:" << soma(10,10)<<endl;
+    cout << "Area do Quadrado: "<< areaQuadrado(5,20)<<endl;
+
+    return 0;
+}
+//implementação  
+int soma(int a, int b){
+
+    return a + b;
+}
+int areaQuadrado(int x, int y){
+    
+    return x * y; 
+
+}
+
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
